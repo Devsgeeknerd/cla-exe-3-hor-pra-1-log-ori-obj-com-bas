9ad41afa07b7c3670891714808d4163af92e85ee
Paulo Vanderley <vanderley.1109@gmail.com>
2024-06-01 17:54:23 -0300

    :page_with_curl: Update file content.

--- Paciente.java
+++ Paciente.java
@@ -25,6 +25,10 @@ public class Paciente {
         this.nome = nome;
     }
 
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
     public Paciente(int codigo, String nome, String sobrenome, int idade, String sexo, String telefone,
             String enderenco) {
         this.codigo = codigo;
